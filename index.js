810ca28f46e0e440523319d34f58d05be0701696
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-19 19:20:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function findMatching(driverArray, name) {
    { return driverName.toLowerCase() ===  name.toLowerCase() });
  }
 
- function fuzzyMatch(driverArray, partialName) {
-    lengthOfPartialName  = partialName
+ function fuzzyMatch(driverArray, partialNam) {
+    let lengthOfPartialName  = partialNam.length;  debugger
     return driverArray.filter(function (driverName)
-    { return driverName.slice(0,lengthOfPartialName).toLowerCase() ===  partialName.toLowerCase() });
+    { return driverName.slice(0,lengthOfPartialName).toLowerCase() ===  partialNam.toLowerCase() });
   }
