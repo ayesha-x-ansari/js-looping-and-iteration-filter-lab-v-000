1ee146be375f74643fa7ae6012be3e472de0aa30
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-08-19 19:02:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function findMatching(driverArray, name) {
    return driverArray.filter(function (driverName)
    { return driverName.toLowerCase() ===  name.toLowerCase() });
  }
+
+ function fuzzyMatch(driverArray, partialName) {
+    lengthOfPartialName  = partialName
+    return driverArray.filter(function (driverName)
+    { return driverName.slice(0,lengthOfPartialName).toLowerCase() ===  partialName.toLowerCase() });
+  }
